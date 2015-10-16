0f35c069ca7d5e7d81d0d8c3ad9814f56ce049b9
Jake Prest <prest_jake@bah.com>
2015-10-16 11:12:12 -0400

    latest

--- js/run.js
+++ js/run.js
@@ -3,26 +3,68 @@ var ctx = $("#myChart").get(0).getContext("2d");
 
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-        {
-            label: "My First dataset",
-            fillColor: "rgba(220,220,220,0.5)",
-            strokeColor: "rgba(220,220,220,0.8)",
-            highlightFill: "rgba(220,220,220,0.75)",
-            highlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
-        },
-        {
-            label: "My Second dataset",
-            fillColor: "rgba(151,187,205,0.5)",
-            strokeColor: "rgba(151,187,205,0.8)",
-            highlightFill: "rgba(151,187,205,0.75)",
-            highlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
-        }
-    ]
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [{
+    label: "My First dataset",
+    fillColor: "rgba(220,220,220,0.5)",
+    strokeColor: "rgba(220,220,220,0.8)",
+    highlightFill: "rgba(220,220,220,0.75)",
+    highlightStroke: "rgba(220,220,220,1)",
+    data: [65, 59, 80, 81, 56, 55, 40]
+  }, {
+    label: "My Second dataset",
+    fillColor: "rgba(151,187,205,0.5)",
+    strokeColor: "rgba(151,187,205,0.8)",
+    highlightFill: "rgba(151,187,205,0.75)",
+    highlightStroke: "rgba(151,187,205,1)",
+    data: [28, 48, 40, 19, 86, 27, 90]
+  }]
 };
 
 var options = {};
 var myBarChart = new Chart(ctx).Bar(data, options);
+
+document.getElementById('txtFileUpload').addEventListener('change', upload, false);
+
+// Method that checks that the browser supports the HTML5 File API
+function browserSupportFileUpload() {
+  var isCompatible = false;
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    isCompatible = true;
+  }
+  return isCompatible;
+}
+
+// Method that reads and processes the selected file
+/*function upload(evt) {
+  if (!browserSupportFileUpload()) {
+    alert('The File APIs are not fully supported in this browser!');
+  } else {
+    var data = null;
+    var file = evt.target.files[0];
+    var reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = function(event) {
+      var csvData = event.target.result;
+      var allTextLines = csvData.split(/\r\n|\n/);
+      var headers = allTextLines[0].split(',');
+      var lines = [];
+
+      for (var i = 1; i < allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+        if (data.length == headers.length) {
+
+          var tarr = [];
+          for (var j = 0; j < headers.length; j++) {
+            tarr.push(headers[j] + ":" + data[j]);
+          }
+          lines.push(tarr);
+        }
+      }
+      console.log(tarr);
+    };
+    reader.onerror = function() {
+      alert('Unable to read ' + file.fileName);
+    };
+  }
+}*/
