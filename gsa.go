69c97205e6fad02e17cd702eb948f08960ae730f
dtrinh100 <dtrinh100@gmail.com>
2015-10-16 12:05:44 -0400

    Loads index.html page

--- gsa.go
+++ gsa.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-    //"github.com/montanaflynn/stats"
+    "github.com/montanaflynn/stats"
     "net/http"
     "github.com/gorilla/mux"
     "log"
+    "strconv"
+    "fmt"
+    "html/template"
 )
 
  func Index(w http.ResponseWriter, r *http.Request) {
-	     w.Write([]byte("Testing API"))
+   t := template.New("index.html") 
+   t, _ = t.ParseFiles("./index.html")  // Parse template file.
+   t.Execute(w, nil)
+}
+
+type DataSet struct {
+  Data []float64
+}
+
+/* Placeholder for data processing function */
+func ProcessData(w http.ResponseWriter, r *http.Request) {
+  var data = []float64{1, 2, 3, 4, 4, 5}
+  median, err := stats.Median(data)
+  if err != nil {
+    log.Println(err)
+  }
+  medianStr := strconv.FormatFloat(median, 'f', 5, 64)
+  fmt.Println(medianStr)
+  w.Write([]byte(medianStr))
 }
 
 func main() {
     r := mux.NewRouter()
-    r.HandleFunc("/api/test", Index)
-    err := http.ListenAndServe(":5000", r)
+    r.HandleFunc("/", Index)
+    r.HandleFunc("/api/process", ProcessData)
+    err := http.ListenAndServe(":7000", r)
     if err != nil {
       log.Fatal(err)
     }
